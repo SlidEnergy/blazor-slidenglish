a0fd6bcf38811df2417c558d8f4ee1c581660b9e
SlidEnergy <slidenergy@gmail.com>
2019-07-27 22:14:27 +1000

    Добавил проект blazor с контекстом базы данных

---
+++ SlidEnglish.Server/Models/Word.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SlidEnglish.Server.Models
+{
+	public class Word
+	{
+		public int Id { get; set; }
+
+		public string Text { get; set; }
+	}
+}
---
+++ SlidEnglish.Server/SlidEnglishContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using SlidEnglish.Server.Models;
+
+namespace SlidEnglish.Server
+{
+    public partial class SlidEnglishContext : DbContext
+    {
+        public SlidEnglishContext()
+        {
+        }
+
+        public SlidEnglishContext(DbContextOptions<SlidEnglishContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseNpgsql("Host=localhost;Database=slidenglish;Username=slidenglishuser;Password=slidenglish");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+
+        public DbSet<SlidEnglish.Server.Models.Word> Word { get; set; }
+    }
+}
